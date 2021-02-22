de2077cd38a671d058e6e61018750a7fe76af708
pRob3 <hundraprocent@me.com>
2021-02-22 18:14:26 +0100

    Get hotel room by id

--- HiddenVilla_API/Controllers/HotelRoomController.cs
+++ HiddenVilla_API/Controllers/HotelRoomController.cs
@@ -1,5 +1,7 @@
 ﻿using Business.Repository.IRepository;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -23,6 +25,35 @@ namespace HiddenVilla_API.Controllers
             var allRooms = await _hotelRoomRepository.GetAllHotelRooms();
 
             return Ok(allRooms);
+        }
+
+        [HttpGet("{roomId}")]
+        public async Task<IActionResult> GetHotelRoom(int? roomId)
+        {
+
+            if(roomId == null)
+            {
+                return BadRequest(new ErrorModel()
+                {
+                    Title ="",
+                    ErrorMessage = "Invalid Room Id",
+                    StatusCode = StatusCodes.Status400BadRequest
+                });
+            }
+
+            var roomDetails = await _hotelRoomRepository.GetHotelRoom(roomId.Value);
+
+            if (roomDetails == null)
+            {
+                return BadRequest(new ErrorModel()
+                {
+                    Title = "",
+                    ErrorMessage = "Invalid Room Id",
+                    StatusCode = StatusCodes.Status404NotFound
+                });
+            }
+
+            return Ok(roomDetails);
         }
     }
 }
