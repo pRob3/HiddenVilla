ad98ce469f7c82922abe934ba7497ff6958227d5
pRob3 <hundraprocent@me.com>
2021-02-22 18:38:25 +0100

    Added Register and Response models Created Account controller with signup

---
+++ HiddenVilla_API/Controllers/AccountController.cs
@@ -0,0 +1,73 @@
+﻿using Common;
+using DataAccess.Data;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HiddenVilla_API.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    [Authorize]
+    public class AccountController : Controller
+    {
+
+        private readonly SignInManager<ApplicationUser> _signInManager;
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+
+        public AccountController(SignInManager<ApplicationUser> signInManager,
+            UserManager<ApplicationUser> userManager,
+            RoleManager<IdentityRole> roleManager)
+        {
+            _signInManager = signInManager;
+            _userManager = userManager;
+            _roleManager = roleManager;
+        }
+
+        [AllowAnonymous]
+        public async Task<IActionResult> SignUp([FromBody] UserRequestDTO userRequestDTO)
+        {
+            if(userRequestDTO == null || !ModelState.IsValid)
+            {
+                return BadRequest();
+            }
+
+            var user = new ApplicationUser
+            {
+                UserName = userRequestDTO.Email,
+                Email = userRequestDTO.Email,
+                Name = userRequestDTO.Name,
+                PhoneNumber = userRequestDTO.PhoneNo,
+                EmailConfirmed = true
+            };
+
+            var result = await _userManager.CreateAsync(user, userRequestDTO.Password);
+
+            if (!result.Succeeded)
+            {
+                var errors = result.Errors.Select(e => e.Description);
+
+                return BadRequest(new RegistrationResponseDTO 
+                                { Errors = errors, IsRegistrationSuccessful = false });                
+            }
+
+            var roleResult = await _userManager.AddToRoleAsync(user, SD.Role_Customer);
+
+            if (!roleResult.Succeeded)
+            {
+                var errors = result.Errors.Select(e => e.Description);
+                return BadRequest(new RegistrationResponseDTO
+                { Errors = errors, IsRegistrationSuccessful = false });
+            }
+
+            return StatusCode(201);
+        }
+        
+    }
+}
