7beb5ecc7ab1253565808b3d3198513f688b1205
pRob3 <hundraprocent@me.com>
2021-02-23 19:01:06 +0100

    Slider images Updated home page

---
+++ HiddenVilla_Client/Model/ViewModel/HomeVM.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HiddenVilla_Client.Model.ViewModel
+{
+    public class HomeVM
+    {
+        public DateTime StartDate { get; set; } = DateTime.Now;
+
+        public DateTime EndDate { get; set; }
+
+        public int NoOfNights { get; set; } = 1;
+    }
+}
