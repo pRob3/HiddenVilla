848ab9ba9a39d41c8dfabd845753fb7fc24e2f7f
pRob3 <hundraprocent@me.com>
2021-02-26 16:29:35 +0100

    Amenities services to API and client

---
+++ HiddenVilla_Client/Service/HotelAmenityService.cs
@@ -0,0 +1,30 @@
+﻿using HiddenVilla_Client.Service.IService;
+using Models;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HiddenVilla_Client.Service
+{
+    public class HotelAmenityService : IHotelAmenityService
+    {
+        private readonly HttpClient _client;
+
+        public HotelAmenityService(HttpClient client)
+        {
+            _client = client;
+        }
+
+        public async Task<IEnumerable<HotelAmenityDTO>> GetHotelAmenities()
+        {
+            var response = await _client.GetAsync($"api/hotelamenity");
+            var content = await response.Content.ReadAsStringAsync();
+            var amenity = JsonConvert.DeserializeObject<IEnumerable<HotelAmenityDTO>>(content);
+            return amenity;
+        }
+    }
+}
