91b947342693a8cd443fff4fa1b92e0ffc5b0ca2
pRob3 <hundraprocent@me.com>
2021-02-28 11:21:24 +0100

    IsRoomBooked service

--- Business/Repository/RoomOrderDetailsRepository.cs
+++ Business/Repository/RoomOrderDetailsRepository.cs
@@ -85,9 +85,23 @@ namespace Business.Repository
             }
         }
 
-        public Task<bool> IsRoomBooked(int roomId, DateTime checkInDate, DateTime checkOutDate)
+        public async Task<bool> IsRoomBooked(int roomId, DateTime checkInDate, DateTime checkOutDate)
         {
-            throw new NotImplementedException();
+            var status = false;
+
+            var existingBooking = await _context.RoomOrderDetails.Where(x => x.RoomId == roomId && x.IsPaymentSuccessfull &&
+            // Check if checkin date that user wants does not fall in between any dataes for that room that is booked
+            (checkInDate < x.CheckOutDate && checkInDate > x.CheckInDate
+            // Check if checkin date that user wants does not fall in between any dataes for that room that is booked
+            || checkOutDate.Date > x.CheckInDate && checkInDate.Date < x.CheckInDate.Date
+            )).FirstOrDefaultAsync();
+
+            if(existingBooking != null)
+            {
+                status = true;
+            }
+
+            return status;
         }
 
         public Task<RoomOrderDetailsDTO> MarkPaymentSuccessful(int id)
