ef4370e7aa18a2a6d53ea4bc86c7f3741d2b1bc4
pRob3 <hundraprocent@me.com>
2021-02-28 11:26:49 +0100

    Room order details service

--- HiddenVilla_Client/Program.cs
+++ HiddenVilla_Client/Program.cs
@@ -26,6 +26,7 @@ namespace HiddenVilla_Client
 
             builder.Services.AddScoped<IHotelRoomService, HotelRoomService>();
             builder.Services.AddScoped<IHotelAmenityService, HotelAmenityService>();
+            builder.Services.AddScoped<IRoomOrderDetailsService, RoomOrderDetailsService>();
 
             await builder.Build().RunAsync();
         }
