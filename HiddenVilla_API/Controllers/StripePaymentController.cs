a1d2dd476f8744a44644a0424e4f2613f3997f7d
pRob3 <hundraprocent@me.com>
2021-03-01 17:36:32 +0100

    Stripe payment controller

---
+++ HiddenVilla_API/Controllers/StripePaymentController.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Models;
+using Stripe.Checkout;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HiddenVilla_API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]/[action]")]
+    public class StripePaymentController : Controller
+    {
+        private readonly IConfiguration _config;
+
+        public StripePaymentController(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        public async Task<IActionResult> Create(StripePaymentDTO payment)
+        {
+            try
+            {
+                var domain = _config.GetValue<string>("HiddenVilla_Client_URL");
+
+                var options = new SessionCreateOptions
+                {
+                    PaymentMethodTypes = new List<string>
+                    {
+                        "card"
+                    },
+                    LineItems=new List<SessionLineItemOptions>
+                    {
+                        new SessionLineItemOptions
+                        {
+                            PriceData = new SessionLineItemPriceDataOptions
+                            {
+                                UnitAmount = payment.Amount*100, // convert to cent
+                                Currency="usd",
+                                ProductData=new SessionLineItemPriceDataProductDataOptions
+                                {
+                                    Name = payment.ProductName
+                                }
+                            },
+                            Quantity = 1
+                        }
+                    },
+                    Mode = "payment",
+                    SuccessUrl = domain + "/success-payment?session_id={{CHECKOUT_SESSION_ID}}",
+                    CancelUrl = domain + payment.ReturnUrl
+                };
+
+                var service = new SessionService();
+                Session session = await service.CreateAsync(options);
+
+                return Ok(new SuccessModel()
+                {
+                    Data = session.Id
+                });
+
+            }
+            catch (Exception e)
+            {
+                return BadRequest(new ErrorModel()
+                {
+                    ErrorMessage = e.Message
+                });
+            }
+
+
+            return View();
+        }
+    }
+}
