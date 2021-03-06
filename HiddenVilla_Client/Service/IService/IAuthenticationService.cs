910024fcf936dc9bff4b2f7bf6d6e746f1a4b5dd
pRob3 <hundraprocent@me.com>
2021-03-06 07:19:43 +0100

    Login service

---
+++ HiddenVilla_Client/Service/IService/IAuthenticationService.cs
@@ -0,0 +1,18 @@
+﻿using Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HiddenVilla_Client.Service.IService
+{
+    public interface IAuthenticationService
+    {
+        Task<RegistrationResponseDTO> RegisterUser(UserRequestDTO userForRegistration);
+
+        Task<AuthenticationResponseDTO> Login(AuthenticationDTO userFromAuthentiocation);
+
+        Task Logout();
+    }
+}
