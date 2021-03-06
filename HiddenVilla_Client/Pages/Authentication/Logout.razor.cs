1cfa3995f15d93c19612f77231353cada7f57747
pRob3 <hundraprocent@me.com>
2021-03-06 17:18:07 +0100

    Notification Helper Methods

---
+++ HiddenVilla_Client/Pages/Authentication/Logout.razor.cs
@@ -0,0 +1,25 @@
+﻿using HiddenVilla_Client.Service.IService;
+using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HiddenVilla_Client.Pages.Authentication
+{
+    public partial class Logout
+    {
+        [Inject]
+        public IAuthenticationService authenticationService { get; set; }
+
+        [Inject]
+        public NavigationManager navMan { get; set; }
+
+        protected async override Task OnInitializedAsync()
+        {
+            await authenticationService.Logout();
+            navMan.NavigateTo("/");
+        }
+    }
+}
--- HiddenVilla_Client/Service/AuthStateProvider.cs
+++ HiddenVilla_Client/Service/AuthStateProvider.cs
@@ -33,5 +33,19 @@ namespace HiddenVilla_Client.Service
             _httpClient.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("bearer", token);
             return new AuthenticationState(new ClaimsPrincipal(new ClaimsIdentity(JWTParser.ParseClaimsFromJwt(token), "jwtAuthType")));
         }
+
+        public void NotifyUserLoggedIn(string token)
+        {
+            var authenticatedUser = new ClaimsPrincipal(new ClaimsIdentity(JWTParser.ParseClaimsFromJwt(token), "jwtAuthType"));
+            var authState = Task.FromResult(new AuthenticationState(authenticatedUser));
+            NotifyAuthenticationStateChanged(authState);
+
+        }
+
+        public void NotifyUserLogout()
+        {
+            var authState = Task.FromResult(new AuthenticationState(new ClaimsPrincipal(new ClaimsIdentity())));
+            NotifyAuthenticationStateChanged(authState);
+        }
     }
 }
