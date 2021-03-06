61d53d57379f82895f9c8aa4e08675d3e3d381c4
pRob3 <hundraprocent@me.com>
2021-03-06 11:23:12 +0100

    Finished register logic

---
+++ HiddenVilla_Client/Pages/Authentication/Register.razor.cs
@@ -0,0 +1,44 @@
+﻿using HiddenVilla_Client.Service.IService;
+using Microsoft.AspNetCore.Components;
+using Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HiddenVilla_Client.Pages.Authentication
+{
+    public partial class Register
+    {
+        private UserRequestDTO userForRegistration = new UserRequestDTO();
+        public bool IsProcessing { get; set; } = false;
+        public bool ShowRegistrationErrors { get; set; }
+        public IEnumerable<string> Errors { get; set; }
+
+        [Inject]
+        public IAuthenticationService authService { get; set; }
+
+        [Inject]
+        public NavigationManager navMan { get; set; }
+
+        private async Task RegisterUser()
+        {
+            ShowRegistrationErrors = false;
+            IsProcessing = true;
+            var result = await authService.RegisterUser(userForRegistration);
+
+            if (result.IsRegistrationSuccessful)
+            {
+                IsProcessing = false;
+                navMan.NavigateTo("/login");
+            }
+            else
+            {
+                IsProcessing = false;
+                Errors = result.Errors;
+                ShowRegistrationErrors = true;
+            }
+        }
+    }
+}
